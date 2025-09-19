93683cc60060ed3cbb96004525a77ec801257033
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-19 10:31:58 +0200

    get_taxi_data, df

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ TODO: add selling/marketing explanations of what this project is about
     - explore uv lock & sync etc.
 
 /file_tree.txt
+
+
+
+
+
+
+setuptools:
+
+uv pip install setuptools
+
+
+uv pip install -e .
--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,7 @@
 from importlib.resources import files
+import pandas as pd
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
-# DATA_PATH = Path(__file__).parents[1] / "data"
+def get_taxi_data():
+    return pd.read_csv(TAXI_CSV_PATH)
