0aae8262a0083f01d7e0f617de39fa52e3b195d4
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-26 14:20:00 +0200

    model testing

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
+CLEAN_CSV_PATH = files("taxipred").joinpath("data/cleaned_data.csv")
+
 def get_taxi_data():
     return pd.read_csv(TAXI_CSV_PATH)
+
+def get_clean_data():
+    return pd.read_csv(CLEAN_CSV_PATH)
